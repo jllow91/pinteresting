03566924fb643a05d43d5b956fd59e1c3e57bd1f
jionglinlow@gmail.com <jionglinlow@gmail.com>
2014-11-16 17:49:27 +0800

    added jQuery masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
+//= require masonry/jquery.masonry
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
